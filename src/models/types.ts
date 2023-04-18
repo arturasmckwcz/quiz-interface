4af43b49e5c9140f6f7178f4b9d46eb268b081b0
Arturas Mickiewicz <arturasmckwcz@gmail.com>
2023-04-19 00:07:21 +0200

    added return type PatientInfo

--- src/models/types.ts
+++ src/models/types.ts
@@ -1,1 +1,19 @@
-export interface Dummy {}
+import { Field, ObjectType } from 'type-graphql';
+
+@ObjectType()
+export class PatientInfo {
+  @Field(() => String, { nullable: true })
+  dob?: string;
+
+  @Field(() => String, { nullable: true })
+  firstName?: string;
+
+  @Field(() => String, { nullable: true })
+  lastName?: string;
+
+  @Field(() => [String], { nullable: true })
+  allergies?: string[];
+
+  @Field(() => [String], { nullable: true })
+  medications?: string[];
+}
